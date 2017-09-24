6ce440e80283dadd1c0a3fa34989c3411eb14e47
Luiz Figueiredo <luizeduard0@iMac-de-Luiz.local>
2017-09-24 10:47:18 -0300

    feat: Posts Component

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CategoriesNav from './../categoriesNav/CategoriesNav'
+import Posts from './../posts/Posts'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <CategoriesNav />
+        <Posts />
       </div>
     );
   }
