f7308c107ea0a79d84c7f7dc48aa4fd47d8a60b1
Luiz Figueiredo <luizeduard0@iMac-de-Luiz.local>
2017-09-24 15:57:16 -0300

    feat: comment timestamp uses momentjs

--- frontend/src/comment/Comment.js
+++ frontend/src/comment/Comment.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Avatar from 'react-avatar'
+import Moment from 'react-moment'
 import './style.css'
 
 const Comment = ({ comment }) => (
   <div className='post-comment'>
-    <div className='post-comment-date'>{comment.timestamp}</div>
+    <div className='post-comment-date'><Moment fromNow>{comment.timestamp}</Moment></div>
     <div className='post-comment-avatar'>
       <Avatar name={comment.author} size={40} round={true} />
     </div>
