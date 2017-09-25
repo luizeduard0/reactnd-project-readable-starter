af646c7c5c3690dbbe89aa193bdfd7760c62073d
Luiz Figueiredo <luizeduard0@iMac-de-Luiz.local>
2017-09-25 08:46:36 -0300

    feat: Comment actions

---
+++ frontend/src/comment/actions.js
@@ -0,0 +1,36 @@
+export const ADD_COMMENT    = 'ADD_COMMENT'
+export const EDIT_COMMENT   = 'EDIT_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const VOTE_COMMENT   = 'VOTE_COMMENT'
+
+export function addComment({ id, body, author, parentId }) {
+  return {
+    type: ADD_COMMENT,
+    id,
+    body,
+    author,
+    parentId
+  }
+}
+
+export function editComment(id, body) {
+  return {
+    type: EDIT_COMMENT,
+    id,
+    body
+  }
+}
+
+export function deleteComment(id) {
+  return {
+    type: DELETE_COMMENT,
+    id
+  }
+}
+
+export function voteComment(id) {
+  return {
+    type: VOTE_COMMENT,
+    id
+  }
+}
