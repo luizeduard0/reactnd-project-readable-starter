7755e2cc776adbf0cdbdeb97660a81ebc482a367
Luiz Figueiredo <luizeduard0@iMac-de-Luiz.local>
2017-09-25 08:56:01 -0300

    fix: comment components location

--- frontend/src/reducer.js
+++ frontend/src/reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import votes from './vote/reducer'
 import posts from './posts/reducer'
-import comments from './comment/reducer'
+import comments from './comments/reducer'
 
 export default combineReducers({
   votes,
