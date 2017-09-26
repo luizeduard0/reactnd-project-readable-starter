3f828ff26db36af0fc642b9bf29a6a7471d41a40
Luiz Figueiredo <luizeduard0@iMac-de-Luiz.local>
2017-09-26 08:19:13 -0300

    feat: PostForm component and post preview structure

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CategoriesNav from './../categoriesNav/CategoriesNav'
 import Posts from './../posts/Posts'
 import Post from './../post/Post'
+import PostForm from './../postForm/PostForm'
 import NoMatch from './../nomatch/NoMatch'
 import { Route, Switch } from 'react-router-dom'
 import './App.css'
@@ -14,6 +15,7 @@ class App extends Component {
         <div className='app-body'>
           <Switch>
             <Route exact path='/' component={Posts} />
+            <Route exact path='/new-post' component={PostForm} />
             <Route path='/:category/posts' component={Posts} />
             <Route path='/:category/:id' component={Post} />
             <Route component={NoMatch} />
---
+++ frontend/src/postForm/PostForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import ContentLoader from 'react-content-loader'
+import './style.css'
+
+class PostForm extends Component {
+  state = {
+    name: 'Untitled',
+    body: null,
+    category: null
+  }
+  render() {
+    return (
+      <div className='post-form'>
+        <div className='row'>
+          <div className='col-md-5'>
+            <h2>Create or Update</h2>
+            <div className='form-group'>
+              <input
+                type="text"
+                name='title'
+                value={this.state.name}
+                className='form-control input-lg'
+                placeholder="Title"
+                onChange={(e) => this.setState({ name: e.target.value })}
+               />
+            </div>
+            <div className='form-group'>
+              <textarea
+                name='body'
+                className='form-control'
+                placeholder="What do you want to say?"
+                onChange={(e) => this.setState({ body: e.target.value })}></textarea>
+            </div>
+            <div className='form-group'>
+              <select className='form-control'>
+                <option disabled>Choose the category</option>
+                <option>react</option>
+                <option>redux</option>
+                <option>udacity</option>
+              </select>
+            </div>
+            <div className='form-group'>
+              <button className='btn btn-success'>Save or Update Post</button>
+            </div>
+          </div>
+          <div className='col-md-7'>
+            <div className='preview-box'>
+              <label className='label label-info pull-right'>PREVIEW</label>
+              <h2>{this.state.name || ';a'}</h2>
+              <p style={{marginTop: '30px'}}>
+                {this.state.body || (
+                  <div>
+                    <small className='preview-tip'>Type on the left to see the preview</small>
+                    <ContentLoader type="list" />
+                  </div>
+                )}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PostForm
