657b70fde92d1327e78c4b24f64e7c9d531d7eab
Luiz Figueiredo <luizeduard0@iMac-de-Luiz.local>
2017-10-01 10:24:34 -0300

    feat: Post Thread shows date

--- frontend/src/postThread/PostThread.js
+++ frontend/src/postThread/PostThread.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Vote from './../vote/Vote'
+import Moment from 'react-moment'
 import './style.css'
 
 class PostThread extends Component {
@@ -12,6 +13,7 @@ class PostThread extends Component {
         <Link to={`/${post.category}/${post.id}`}>
           <h3 className='post-thread-title'>{ post.title }</h3>
           <small className='post-thread-author'>{ post.author }</small>
+          <small className='post-thread-date'> &bull; <Moment fromNow>{post.timestamp }</Moment></small>
         </Link>
       </div>
     )
