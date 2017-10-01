60367fcd66793cc6ea0b30f00b5680c495911729
Luiz Figueiredo <luizeduard0@iMac-de-Luiz.local>
2017-10-01 10:42:30 -0300

    feat: Posts can be edit/delete from listing page

--- frontend/src/postThread/PostThread.js
+++ frontend/src/postThread/PostThread.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import * as PostApi from './../posts/api'
+import { deletePost } from './../posts/actions'
+import { notify } from './../notificationSystem/actions'
+import { uuid } from './../utils/helpers'
 import Vote from './../vote/Vote'
 import Moment from 'react-moment'
 import './style.css'
 
 class PostThread extends Component {
+  deletePost = id => {
+    PostApi.deletePost(id)
+      .then(response => {
+        if(response.deleted) {
+          this.props.dispatch(deletePost(id))
+          this.props.dispatch(notify({
+            id: uuid(),
+            type: 'success',
+            message: 'The post was deleted'
+          }))
+        }
+      })
+    }
   render() {
     const { post } = this.props
     return (
       <div className='post-thread'>
-        <Vote id={post.id} score={post.voteScore} type='post' />
+        <div className='post-thread-actions'>
+          <Vote id={post.id} score={post.voteScore} type='post' />
+          <Link to={`/post/${post.id}/edit`} className='btn btn-link editPost' style={{ marginRight: '15px', borderRight: '1px solid #eee' }}>
+            <i className='glyphicon glyphicon-pencil'></i> Edit
+          </Link>
+          <button
+            onClick={() => this.deletePost(post.id)}
+            className='btn btn-danger deletePost'
+            style={{ background: 'transparent', color: '#d43f3a' }}>
+            <i className='glyphicon glyphicon-trash'></i> Delete
+          </button>
+        </div>
         <Link to={`/${post.category}/${post.id}`}>
           <h3 className='post-thread-title'>{ post.title }</h3>
           <small className='post-thread-author'>{ post.author }</small>
@@ -20,4 +49,4 @@ class PostThread extends Component {
   }
 }
 
-export default PostThread
+export default connect()(PostThread)
